a30f045477f2d02bd0f338266db40117df239112
dvnf10cpp <127529254+dvnf10cpp@users.noreply.github.com>
2026-05-13 19:03:25 +0700

    Create hackerrank_pembagian_kelompok.cpp

---
+++ hackerrank_pembagian_kelompok.cpp
@@ -0,0 +1,12 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    long double n, m; cin >> n >> m; cout << (long long)ceil(n / m);
+    return 0;
+}
